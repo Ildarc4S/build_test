64d8ea524c4302a470aed6096634b4f8ec675d08
Ildarc4S <ildarc4S@gmail.com>
2025-06-30 17:15:24 +0300

    First steps

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <boost/algorithm/string.hpp>
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string s = "hello world";
+  boost::to_upper(s);
+  std::cout << s << std::endl;
+  return 0;
+}
+
